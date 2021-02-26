a4b23f7a79e43ba957831ff4c1f8626fe13e746b
Rachel Martinez <rachelmartinez101@gmail.com>
2021-02-26 11:57:31 -0700

    Complete Part 2

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -6,11 +6,16 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental', function(hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.owner.setupRouter();
+  });
+
     test('it renders information about a rental property', async function (assert) {
 //    await render(hbs`<Rental />`); // this line ruins lives
 
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -26,10 +31,11 @@ module('Integration | Component | rental', function(hooks) {
       }
     });
 
-    await render(hbs`<Rental @rental={{this.rental}} />`);
+//    await render(hbs`<Rental @rental={{this.rental}} />`);
 
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert.dom('article h3 a').hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
